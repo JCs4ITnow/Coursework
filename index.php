4479b28c66a55b88a5abf210dca5dfb633aa8376
JCs4ITnow <44196937+JCs4ITnow@users.noreply.github.com>
2018-10-25 09:13:44 +0100

    Added index php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+  <form action="logreg.php" method="post">
+    Username: <input type="text" name="username"><br>
+    Password: <input type="password" name="password"><br><br>
+  <input type="submit" name="login" value="Login">
+  <input type="submit" name="register" value="Register">
+</form>
+
+</body>
+</html>
